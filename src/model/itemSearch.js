65f8c8e68a5af671b7172bf1db0bc786fbf36c54
Hugo Silva <hugo.matos.silva00@gmail.com>
2023-11-22 00:29:18 -0300

    Ajuste na busca de item

--- src/model/itemSearch.js
+++ src/model/itemSearch.js
@@ -7,9 +7,14 @@ let con
 async function itemSearch(item){
   try {
     con = await dbConnect();
-    response = await Item.findById(item.id);
+    let byID
+    let byAuthor = []
+    let byTitle = []
+    byID = await Item.findById(item.id);
+    byAuthor = await Item.find({author: item.author});
+    byTitle = await Item.find({title: item.title});
     await con.connection.close();
-    return response;
+    return [byID, ...byAuthor, ...byTitle];
   } catch (err) {
     await con.connection.close();
     if (err.path == "_id") throw {statusCode: 400, message: "Item não encontrado"}
